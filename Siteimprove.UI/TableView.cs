b1116dd8b8d63fdf401dc52fe1261ba896d0069f
Mads Jacobsen <mja@siteimprove.com>
2012-02-13 16:00:09 +0100

    Added a control to master

---
+++ Siteimprove.UI/TableView.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Siteimprove.UI
+{
+	[DefaultProperty("Text")]
+	[ToolboxData("<{0}:TableView runat=server></{0}:TableView>")]
+	public class TableView : WebControl
+	{
+		[Bindable(true)]
+		[Category("Appearance")]
+		[DefaultValue("")]
+		[Localizable(true)]
+		public string Text
+		{
+			get
+			{
+				String s = (String)ViewState["Text"];
+				return (s ?? String.Empty);
+			}
+
+			set
+			{
+				ViewState["Text"] = value;
+			}
+		}
+
+		protected override void RenderContents(HtmlTextWriter output)
+		{
+			output.Write(Text);
+		}
+	}
+}
