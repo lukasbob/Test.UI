ecea8aa54cf43d43e155195806af421ed2650ae6
Mads Jacobsen <mja@siteimprove.com>
2012-02-14 13:13:33 +0100

    TableView changes.

--- Siteimprove.UI/TableView.cs
+++ Siteimprove.UI/TableView.cs
@@ -1,35 +1,129 @@
 ﻿using System;
-using System.ComponentModel;
+using System.Collections;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Siteimprove.Extensions.HtmlTextWriter;
 
 namespace Siteimprove.UI
 {
-	[DefaultProperty("Text")]
 	[ToolboxData("<{0}:TableView runat=server></{0}:TableView>")]
-	public class TableView : WebControl
+	public class TableView : CompositeDataBoundControl
 	{
-		[Bindable(true)]
-		[Category("Appearance")]
-		[DefaultValue("")]
-		[Localizable(true)]
-		public string Text
-		{
-			get
-			{
-				String s = (String)ViewState["Text"];
-				return (s ?? String.Empty);
-			}
+		[PersistenceMode(PersistenceMode.InnerProperty), TemplateContainer(typeof(Row))]
+		public ITemplate RowTemplate { get; set; }
 
-			set
-			{
-				ViewState["Text"] = value;
-			}
+		/// <summary>
+		/// Delegates event handling to bound event handlers.
+		/// </summary>
+		/// <param name="sender">The sender.</param>
+		/// <param name="e">The <see cref="TableViewEventArgs"/> instance containing the event data.</param>
+		public delegate void TableViewItemEventHandler(object sender, TableViewEventArgs e);
+
+		/// <summary>
+		/// Occurs when the <see cref="Row"/> is created.
+		/// </summary>
+		public event TableViewItemEventHandler ItemCreated;
+
+		/// <summary>
+		/// Occurs when data is bound to the <see cref="Row"/>.
+		/// </summary>
+		public event TableViewItemEventHandler ItemDataBound;
+
+		private bool _dataBound;
+
+		/// <summary>
+		/// Raises the <see cref="ItemCreated"/> event.
+		/// </summary>
+		/// <param name="e">The <see cref="TableViewEventArgs"/> instance containing the event data.</param>
+		public void OnItemCreated(TableViewEventArgs e)
+		{
+			TableViewItemEventHandler handler = ItemCreated;
+			if (handler != null) { handler(this, e); }
 		}
 
-		protected override void RenderContents(HtmlTextWriter output)
+		/// <summary>
+		/// Raises the <see cref="ItemDataBound"/> event.
+		/// </summary>
+		/// <param name="e">The <see cref="TableViewEventArgs"/> instance containing the event data.</param>
+		public void OnItemDataBound(TableViewEventArgs e)
 		{
-			output.Write(Text);
+			TableViewItemEventHandler handler = ItemDataBound;
+			if (handler != null) { handler(this, e); }
+		}
+
+		protected override void Render(HtmlTextWriter writer)
+		{
+			if (!_dataBound) { DataBind(); }
+			base.Render(writer);
+		}
+
+		protected override int CreateChildControls(IEnumerable dataSource, bool dataBinding)
+		{
+			var count = 0;
+			if (dataBinding) {
+				Controls.Clear();
+
+
+
+				foreach (var item in dataSource) {
+					var row = new Row(item, count);
+					var rowEvent = new TableViewEventArgs(row);
+
+					if (RowTemplate != null) {
+						RowTemplate.InstantiateIn(row);
+					}
+
+					OnItemCreated(rowEvent); // Raise the OnItemCreated event.
+					row.DataBind();
+					OnItemDataBound(rowEvent); // Raise the OnItemDataBound event.
+
+					Controls.Add(row);
+
+					count++;
+				}
+
+				_dataBound = true;
+			}
+
+			return count;
 		}
 	}
+
+	public class Row : BaseControl, IDataItemContainer
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Row"/> class.
+		/// </summary>
+		/// <param name="dataItem">The data item.</param>
+		/// <param name="index">The index.</param>
+		public Row(object dataItem, int index)
+		{
+			DataItem = dataItem;
+			DataItemIndex = index;
+		}
+
+		#region Implementation of IDataItemContainer
+
+		public object DataItem { get; private set; }
+		public int DataItemIndex { get; private set; }
+		public int DisplayIndex { get; private set; }
+
+		#endregion
+
+		protected override void Render(HtmlTextWriter writer)
+		{
+			writer.Tag("tr", e => e["class", CssClass, !string.IsNullOrEmpty(CssClass)])
+				.Do(RenderChildren)
+			.EndTag("tr");
+		}
+	}
+
+
+	public class TableViewEventArgs : EventArgs
+	{
+		public TableViewEventArgs(Row item) { Item = item; }
+
+		public Row Item { get; private set; }
+	}
+
 }
