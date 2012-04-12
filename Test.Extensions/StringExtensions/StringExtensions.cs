c075b8cb6c8836fee2b45c2cd05b9c462dd6f5e3
Mads Jacobsen <lukasbob@gmail.com>
2012-04-12 15:16:01 +0200

    Added an enum parsing extension method.

--- Test.Extensions/StringExtensions/StringExtensions.cs
+++ Test.Extensions/StringExtensions/StringExtensions.cs
@@ -1,4 +1,5 @@
-﻿namespace Test.Extensions.StringExtensions {
+﻿using System;
+namespace Test.Extensions.StringExtensions {
 	public static class StringExtensions {
 		/// <summary>
 		/// Determines whether the specified input string is null or empty.
@@ -9,6 +10,31 @@
 		/// </returns>
 		public static bool IsNullOrEmpty(this string inputString) {
 			return string.IsNullOrEmpty(inputString);
+		}
+		
+		/// <summary>
+		/// Parses the current string as an enum value. Failure to parse the input string returns the default value.
+		/// </summary>
+		/// <returns>
+		/// The enum.
+		/// </returns>
+		/// <param name='inputString'>
+		/// Input string.
+		/// </param>
+		/// <param name='defaultValue'>
+		/// Default value.
+		/// </param>
+		/// <typeparam name='T'>
+		/// An Enum type parameter.
+		/// </typeparam>
+		public static T AsEnum<T>(this string inputString, T defaultValue) {
+			try {
+				var val = Enum.Parse(typeof(T), inputString);
+				return (T)val;
+			} catch (Exception ex) {
+				return defaultValue;
+			}
+
 		}
 	}
 }
